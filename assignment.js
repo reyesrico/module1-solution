6ea51107b0bf0b1c9de7cb9c3e97b2120324abc8
Carlos Reyes <reyesrico@hotmail.com>
2017-11-28 01:05:00 -0800

    Implementing optional bonus.

--- assignment.js
+++ assignment.js
@@ -11,12 +11,18 @@
             var food = $scope.food;
             if (food === undefined || food === '') {
                 $scope.result = "Please enter data first";
+                $scope.class = "text-danger";
+                $scope.fclass = "red";
             }
             else if (food.split(',').length > 3) {
                 $scope.result = "Too Much!";
+                $scope.class = "text-success";
+                $scope.fclass = "green";
             }
             else {
                 $scope.result = "Enjoy!";
+                $scope.class = "text-success";
+                $scope.fclass = "green";
             }
         }
     }
